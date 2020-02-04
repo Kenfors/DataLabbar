1ef6438481b57b3cd5d319d2c0ed685d8748fdae
KaEnfors <37064780+KaEnfors@users.noreply.github.com>
2020-02-04 21:36:02 +0100

    upg5 fungerar

--- AlgoritmerDatastrukturer/ADKJavaLabbar/src/labb2/PackageSorter.java
+++ AlgoritmerDatastrukturer/ADKJavaLabbar/src/labb2/PackageSorter.java
@@ -2,27 +2,76 @@ package labb2;
 
 public class PackageSorter {
 	
-	
+	private int MAX;
+	private String lastResult;
+	private String lastLetters;
 	
 	public PackageSorter() {
-		
-		
-	}
-	
-	public void run(String letters) {
-		
+		this.MAX = 13;
+		this.lastLetters = "NONE";
+		this.lastResult = "";
 		
 	}
 	
-	private int solve(int steps) {
+	public void run(String startingorder) {
+		this.lastLetters = startingorder;
+		char[] letters = startingorder.toCharArray();
 		
-		if(isDone())
-			return steps;
-		else return solve(steps);
+		String shift = this.shift(letters.clone(), "", 0);
+		String flip = this.flip(letters.clone(), "");
+		this.lastResult = (shift.length() < flip.length()) ? shift : flip;
 	}
 	
-	private boolean isDone() {
+	public String toString() {
+		String verbal = "Ordning: " + this.lastLetters;
+		verbal += "\nTar " + this.lastResult.length() + " steg";
+		return verbal;
+	}
+	
+	private String shift(char[] letters, String moves, int halt) {
+		if(halt >= 4) return "------------------------------------------------------";
+		//System.out.println(moves + new String(letters));
+		if(this.isDone(letters)) {
+			System.out.println("Complete: " + moves);
+			return moves;
+		}
+		char tmp = letters[4];
+		letters[4] = letters[3];
+		letters[3] = letters[2];
+		letters[2] = letters[1];
+		letters[1] = letters[0];
+		letters[0] = tmp;
 		
+		String shift = this.shift(letters.clone(), new String(moves + "s"), halt + 1);
+		String flip = this.flip(letters.clone(), new String(moves + "b"));
+		//System.out.println((shift.length() < flip.length()) ? shift : flip);
+		return (shift.length() < flip.length()) ? shift : flip;
+	}
+	
+	private String flip(char[] letters, String moves) {
+		//System.out.println(moves + new String(letters));
+		if(this.isDone(letters)) {
+			System.out.println("Complete: " + moves);
+			return moves;
+		}
+		if(moves.length() >= MAX) return "------------------------------------------------------";
+		
+		char tmp = letters[0];
+		letters[0] = letters[1];
+		letters[1] = tmp;
+
+		String s = this.shift(letters.clone(), new String(moves + "s"), 0);
+		//System.out.println(moves);
+		//System.out.println(s);
+		return s;
+	}
+	
+	private boolean isDone(char[] letters) {
+		for(int i=0; i< letters.length-1; i++) {
+			if(letters[i] > letters[i+1]) return false;
+		}
+		System.out.println("#####################################################################Done!");
+		return true;
 	}
 	
 	
--- AlgoritmerDatastrukturer/ADKJavaLabbar/src/labb2/RED2.java
+++ AlgoritmerDatastrukturer/ADKJavaLabbar/src/labb2/RED2.java
@@ -4,10 +4,25 @@ public class RED2 {
 
 	public static void main(String[] args) {
 
-
+		upg5();
+		
+	}
+	
+	public static void upg4() {
+		
 		Airport2 planes = new Airport2();
-		planes.runSim(10);
+		planes.runSim(1000);
 		//planes.runTest();
+		
+	}
+	
+	public static void upg5() {
+		
+		PackageSorter pkt = new PackageSorter();
+		
+		pkt.run("BECAD");
+		System.out.println(pkt);
+		
 	}
 
 }
